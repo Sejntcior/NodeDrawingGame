e9a594f72255528aa1a7aea044dde6b4634bc5f4
Sejntcior <sejntcik@gmail.com>
2015-03-16 19:11:04 +0100

    works with newest nodejs

--- app.js
+++ app.js
@@ -7,42 +7,23 @@
 
 // Including libraries
 
-var app = require('http').createServer(handler),
-	io = require('socket.io').listen(app),
-	nstatic = require('node-static'); // for serving files
+var nstatic = require('node-static');
+var fileServer = new(nstatic.Server)('./');
+var http = require('http');
 
-// This will make all the files in the current folder
-// accessible from the web
-var fileServer = new nstatic.Server('./');
-	
-// This is the port for our web server.
-// you will need to go to http://localhost:3000 to see it
-var port = process.env.PORT || 80; // Cloud9 + Heroku || localhost
-app.listen(port);
-
-// If the URL of the socket server is opened in a browser
-function handler (request, response) {
-	request.addListener('end', function () {
+var app = http.createServer(function (request, response) {
+    request.resume();
+    request.addListener('end', function () {
         fileServer.serve(request, response);
     });
-}
+});
 
-// Delete this row if you want to see debug messages
-io.set('log level', 1);
+var io = require('socket.io')(app);
 
-// Heroku doesn't support websockets so...
-// Detect if heroku via config vars
-// https://devcenter.heroku.com/articles/config-vars
-// heroku config:add HEROKU=true --app node-drawing-game
-if (process.env.HEROKU === 'true') {
-    io.configure(function () {
-        io.set("transports", ["xhr-polling"]);
-        io.set("polling duration", 10);
-    });
-}
+app.listen(8000);
 
 // Listen for incoming connections from clients
-io.sockets.on('connection', function (socket) {
+io.on('connection', function (socket) {
 
 	// Start listening for mouse move events
 	socket.on('mousemove', function (data) {
